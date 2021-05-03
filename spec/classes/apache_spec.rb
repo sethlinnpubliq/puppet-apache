b2085e37359c6ff0473a4450b767c95ce50decce
root <root@b9be222de51c.mylabserver.com>
2021-05-03 07:50:10 +0000

    Added unit tests

--- spec/classes/apache_spec.rb
+++ spec/classes/apache_spec.rb
@@ -6,8 +6,20 @@ describe 'apache' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-
+      it { is_expected.to contain_class('apache::install') }
+      it { is_expected.to contain_class('apache::config') }
+      it { is_expected.to contain_class('apache::service') }
       it { is_expected.to compile }
+
+      case os_facts[:os_family]
+      when 'Debian'
+        it { is_expected.to contain_package('apache2').with_ensure('present') }
+        it { is_expected.to contain_service('apache2').with(ensure: 'running', enable: true, hasrestart: true) }
+      when 'RedHat'
+        it { is_expected.to contain_package('httpd').with_ensure('present') }
+        it { is_expected.to contain_service('httpd').with(ensure: 'running', enable: true, hasrestart: true) }
+      end
+
     end
   end
 end
